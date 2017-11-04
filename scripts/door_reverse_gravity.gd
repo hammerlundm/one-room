52a607b4d56f064754d2cb7e45b4b88d54ef9549
Max Hammerlund <hammerlundm@gmail.com>
2017-11-04 14:55:38 -0500

    Fix crash in reverse_gravity effect

--- scripts/door_reverse_gravity.gd
+++ scripts/door_reverse_gravity.gd
@@ -2,4 +2,3 @@ extends "door.gd"
 
 func apply():
 	INIT_POS = Vector2(256, 0)
-	sprite.scale(Vector2(1, -1))
--- scripts/player_reverse_gravity.gd
+++ scripts/player_reverse_gravity.gd
@@ -4,3 +4,4 @@ func apply():
 	G = Vector2(0.0, -1.0)
 	INIT_POS = Vector2(1700, 900)
 	JUMP_HEIGHT = -1.0
+	sprite.scale(Vector2(1, -1))
