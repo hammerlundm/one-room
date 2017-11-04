04eafe45aaf3232dd401bd83c459259cac8a4b30
Max Hammerlund <hammerlundm@gmail.com>
2017-11-03 21:42:05 -0500

    Add basic player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,38 @@
 extends KinematicBody2D
 
+#Variables that can be changed
+var G = Vector2(0.0, 1.0)
+var SPEED = 1.0
+var JUMP_HEIGHT = 1.0
+var FRICTION = 1.0
+
+#other variables
+var v = Vector2(0, 0)
+var a = Vector2(0, 0)
+export(float) var SPEED_FACTOR = 100.0
+export(float) var FRICTION_FACTOR = 5.0
+export(float) var GRAVITY_FACTOR = 5.0
+
 func _ready():
 	set_fixed_process(true)
 
-func _fixed_process(delta):
+func update_pos(delta):
+	v += a * delta
+	var f = pow(0.5, FRICTION * FRICTION_FACTOR * delta)
+	v *= f
+	a *= f*f
+	move(v)
+
+func do_gravity(delta):
+	v += G * GRAVITY_FACTOR * delta
+
+func get_input(delta):
 	if Input.is_action_pressed("ui_left"):
-		move(Vector2(-10, 0))
-	elif Input.is_action_pressed("ui_right"):
-		move(Vector2(10, 0))
+		a.x -= SPEED * SPEED_FACTOR * delta
+	if Input.is_action_pressed("ui_right"):
+		a.x += SPEED * SPEED_FACTOR * delta
+
+func _fixed_process(delta):
+	update_pos(delta)
+	do_gravity(delta)
+	get_input(delta)
