afcae1a7e4951bb550b7975af73c9bd2234b6eae
Max Hammerlund <hammerlundm@gmail.com>
2017-11-04 09:12:57 -0500

    Make room.tscn easier to modify

--- scripts/room.gd
+++ scripts/room.gd
@@ -1,24 +1,37 @@
 extends Control
 
+#Variables that can be changed
+var PLAYER_POS = Vector2(0, 0)
+var DOOR_POS = Vector2(480, 640)
+
+#other variables
 var player_scene = preload("res://scenes/player.tscn")
+var door_scene = preload("res://scenes/door.tscn")
 var player = null
+var door = null
 
 var levels = ["start", "0g", "fast", "ice", "reverse_controls"]
 var current = 0
 
-func thing(body):
+func player_enter_door(body):
 	if body == player:
-		print("!!!")
 		current += 1
+		load_level(current)
+
+func load_level(level):
+	if player:
 		player.queue_free()
-		player = player_scene.instance()
-		var script = load("res://scripts/level_" + levels[current] + ".gd")
-		player.set_script(script)
-		add_child(player)
-		print(levels[current])
+	player = player_scene.instance()
+	var player_script = load("res://scripts/player_" + levels[level] + ".gd")
+	player.set_script(player_script)
+	player.set_pos(PLAYER_POS)
+	add_child(player)
+	if door:
+		door.queue_free()
+	door = door_scene.instance()
+	door.set_pos(DOOR_POS)
+	door.connect("body_enter", self, "player_enter_door")
+	add_child(door)
 
 func _ready():
-	player = player_scene.instance()
-	add_child(player)
-	var door = get_node("door")
-	door.connect("body_enter", self, "thing")
+	load_level(0)
