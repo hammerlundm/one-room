cbf814c9a8d0cbb097869437f801e98235761692
= <=>
2017-11-04 18:53:42 -0500

    Make music level 0g, fixed player hitbox

--- scripts/player_sonar.gd
+++ scripts/player_sonar.gd
@@ -4,6 +4,26 @@ var door_dist
 var vol
 var music
 
+func get_input(delta):
+	if Input.is_action_pressed("ui_left"):
+		a.x -= SPEED * SPEED_FACTOR * delta
+	if Input.is_action_pressed("ui_right"):
+		a.x += SPEED * SPEED_FACTOR * delta
+	if Input.is_action_pressed("ui_up"):
+		a.y -= SPEED * SPEED_FACTOR * delta
+	if Input.is_action_pressed("ui_down"):
+		a.y += SPEED * SPEED_FACTOR * delta
+
+func do_friction(delta):
+	var f = pow(0.5, FRICTION * FRICTION_FACTOR * delta)
+	v *= f
+	a *= f*f
+
+func apply():
+  G = Vector2(0, 0)
+  JUMP_HEIGHT = 0
+  sprite.play("jump")
+
 func _ready():
   ._ready()
   music = get_parent().get_node("StreamPlayer")
